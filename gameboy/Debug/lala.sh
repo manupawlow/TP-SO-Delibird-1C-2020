ecac6a02c7b7dbd4348eeb5aff8edca3a3d36c3e
matiezequiel <maatiaasezequiel@hotmail.com>
2020-07-17 22:30:25 -0300

    De nuevo..

---
+++ gameboy/Debug/lala.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./gameboy BROKER NEW_POKEMON Pikachu 9 9 3
+./gameboy BROKER NEW_POKEMON Squirtle 6 7 3
+./gameboy BROKER NEW_POKEMON Onix 2 8 3
+./gameboy BROKER NEW_POKEMON Gengar 6 6 3
+./gameboy BROKER NEW_POKEMON Squirtle 5 5 3
